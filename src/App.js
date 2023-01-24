247033c6c35a9c2698057e8dd57b195cdb1b7b4a
John Moore <17406659+PinKushin@users.noreply.github.com>
2023-01-24 11:57:23 -0500

    add needed IDs and a Button to fetch a new quote

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ class App extends Component {
     render() {
         return (
             <div className='App'>
-                <header className='App-header'>
+                <div className='App-header' id='quote-box'>
                     <img src={logo} className='App-logo' alt='logo'/>
-                    <p>
+                    <p id='text'>
                         Edit <code>src/App.js</code> and save to reload.
                     </p>
+                    <button id='new-quote' onClick=''>New Quote</button>
                     <a
                         className='App-link'
-                        href='https://reactjs.org'
+                        href='twitter.com/intent/tweet'
                         target='_blank'
                         rel='noopener noreferrer'
+                        id='tweet-quote'
                     >
                         Learn React
                     </a>
-                </header>
+                </div>
             </div>
         );
     }
