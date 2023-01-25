053e20477d822b2fb6b58e2a6dcc299b5c749fc2
John Moore <17406659+PinKushin@users.noreply.github.com>
2023-01-25 10:33:32 -0500

    finish random quote page
    Signed-off-by: John Moore <17406659+PinKushin@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,35 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
-import {Component} from 'react';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-class App extends Component {
-    newQuote() {
+export default function App()
+{
+    const [ data, setData ] = React.useState( null );
 
+    async function updateQuote()
+    {
+        try
+        {
+            const response = await fetch( 'https://api.quotable.io/random' );
+            const { statusCode, statusMessage, ...data } = await response.json();
+            setData( data );
+        } catch( error )
+        {
+            // If the API request failed, log the error to console and update state
+            // so that the error will be reflected in the UI.
+            console.error( error );
+            setData( { content: 'Something went wrong' } );
+        }
     }
 
-    render() {
-        return (
-            <div className='App'>
-                <div className='App-header' id='quote-box'>
-                    <img src={logo} className='App-logo' alt='logo'/>
-                    <p id='text'>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <p id='author'> - Anon</p>
-                    <button id='new-quote' onClick=''>New Quote</button>
-                    <a
-                        className='App-link'
-                        href='https://www.twitter.com/intent/tweet'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        id='tweet-quote'
-                    >
-                        Share Quote on Twitter
-                    </a>
-                </div>
-            </div>
-        );
-    }
+    // Run `updateQuote` once when component mounts
+    React.useEffect( () =>
+                     {
+                         updateQuote();
+                     }, [] );
+
+    // Do not render until the first quote is loaded
+    if ( !data ) return null;
+
+    return (
+        <div className='App' id='quote-box'>
+            <Card style={ { width: '90%', maxWidth: '40rem' } }>
+                <Card.Body>
+                    <blockquote className='blockquote mb-0'>
+                        <p id='text'>{ data.content }</p>
+                        { data.author && (
+                            <footer className='blockquote-footer'>
+                                <cite title='Source Title' id='author'>{ data.author }</cite>
+                            </footer>
+                        ) }
+                    </blockquote>
+                </Card.Body>
+                <Card.Footer>
+                    <Button id='new-quote' variant='primary' onClick={ updateQuote }>
+                        New Quote
+                    </Button>
+                    <a href='https://www.twitter.com/intent/tweet' className='App-link' id='tweet-quote'>Share Quote on
+                        Twitter</a>
+                </Card.Footer>
+            </Card>
+        </div>
+    );
 }
-
-export default App;
